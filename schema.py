c3541a227c175f696dc8bfccaa21359799099cbd
codexlynx <codexlynx@gmail.com>
2017-07-09 23:33:44 +0100

    feat(graphene-etc-protocols): Add etc_protocols schema

--- schema.py
+++ schema.py
@@ -10,6 +10,12 @@ class Cpuid(graphene.ObjectType):
     output_bit = graphene.String()
     input_eax = graphene.String()
 
+class EtcProtocols(graphene.ObjectType):
+    name = graphene.String()
+    number = graphene.String()
+    alias = graphene.String()
+    comment = graphene.String()
+
 class Query(graphene.ObjectType):
     cpuid = graphene.List(Cpuid)
 
@@ -21,6 +27,17 @@ class Query(graphene.ObjectType):
                     output_register=item['output_register'],
                     output_bit=item['output_bit'],
                     input_eax=item['input_eax']
+            )
+
+    etc_protocols = graphene.List(EtcProtocols)
+
+    def resolve_etc_protocols(self, args, context, info):
+        for item in query.run('select * from etc_protocols'):
+            yield EtcProtocols(
+                    name=item['name'],
+                    number=item['number'],
+                    alias=item['alias'],
+                    comment=item['comment']
             )
 
 schema = graphene.Schema(query=Query)
